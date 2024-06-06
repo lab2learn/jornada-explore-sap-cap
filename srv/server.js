3d7e31f2fdc9f06b06dd14615eca2569da6ed5fe
Carlos Santos <carlos.santos@lab2dev.com>
2024-06-06 14:00:59 +0000

    feat: add cors

--- srv/server.js
+++ srv/server.js
@@ -1,6 +1,18 @@
+"use strict";
+
 const cds = require("@sap/cds");
+const cors = require('cors');
 const proxy = require("@sap/cds-odata-v2-adapter-proxy");
 
-cds.on("bootstrap", app => app.use(proxy()));
+cds.on("bootstrap", app => {
+    app.use(proxy());
+
+    app.use(cors({
+        origin: '*', // Allow all origins for development; restrict in production
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+        allowedHeaders: ['Content-Type', 'Authorization'],
+        credentials: true
+    }));
+});
 
 module.exports = cds.server;
